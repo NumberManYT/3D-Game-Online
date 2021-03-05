ac57db7a28c6e2545b18b54db9084e99a3a36bba
NumberManYT <63100872+NumberManYT@users.noreply.github.com>
2021-03-05 23:22:22 +0700

    Mekanik FPS

---
+++ 3D Game Onlen/Assets/GroundSensor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    public PlayerMovement PM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            PM.IsGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            PM.IsGrounded = false;
+        }
+    }
+}
---
+++ 3D Game Onlen/Assets/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    public float SpeedRun;
+    public float JumpForce;
+    public float Sensifity;
+    public GameObject Camera;
+    public bool IsGrounded;
+
+
+
+    float VerticalLookRotation;
+    float SpeedBank;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        SpeedBank = Speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CameraRotate();
+    }
+
+    void Move()
+    {
+
+        //Movement
+        float Horiz = Input.GetAxisRaw("Horizontal");
+        float Vert = Input.GetAxisRaw("Vertical");
+
+        Vector3 Pos = new Vector3(Horiz, Vert, transform.position.z).normalized;
+
+        transform.Translate(Vector3.forward * Vert * Speed * Time.deltaTime);
+        transform.Translate(Vector3.right * Horiz * Speed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            Speed = SpeedRun;
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Speed = SpeedBank;
+        }
+
+        //Jump
+        if(Input.GetKey(KeyCode.Space) && IsGrounded)
+        {
+            rb.velocity = new Vector3(0, JumpForce, 0);
+        }
+    }
+
+    void CameraRotate()
+    {
+        float PosX = Input.GetAxisRaw("Mouse X") * Sensifity;
+        VerticalLookRotation += Input.GetAxisRaw("Mouse Y") * Sensifity;
+
+        transform.Rotate(Vector3.up * PosX);
+
+
+        VerticalLookRotation = Mathf.Clamp(VerticalLookRotation, -70f, 90f);
+        Camera.transform.localEulerAngles = Vector3.left * VerticalLookRotation;
+    }
+
+}
